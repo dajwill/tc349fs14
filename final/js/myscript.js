012c3d94b6463675524d173960007ff167c5cb16
dajwill <will2471@msu.edu>
2014-12-10 22:51:40 -0500

    Final project push

---
+++ final/js/myscript.js
@@ -0,0 +1,40 @@
+$(function(){ // document ready
+ 
+  if (!!$('.sticky').offset()) { // make sure ".sticky" element exists
+ 
+    var stickyTop = $('.sticky').offset().top; // returns number 
+ 
+    $(window).scroll(function(){ // scroll event
+ 
+      var windowTop = $(window).scrollTop(); // returns number 
+ 
+      if (stickyTop < windowTop){
+        $('.sticky').css({ position: 'fixed', top: 0 });
+      }
+      else {
+        $('.sticky').css('position','static');
+      }
+ 
+    });
+ 
+  }
+ 
+});
+
+$(document).ready(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  
+
